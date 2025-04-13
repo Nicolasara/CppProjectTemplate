4f53ca7ea65396a294219790684bec67096c8aa5
nicoaraujo <araujo.n@northeastern.edu>
2025-04-12 17:14:01 -0700

    Add commands to run the application in the make file

--- Scripts/Setup-Linux.sh
+++ Scripts/Setup-Linux.sh
@@ -3,3 +3,9 @@
 pushd ..
 Vendor/Binaries/Premake/Linux/premake5 --cc=clang --file=Build.lua gmake2
 popd
+
+echo "" >> ../Makefile
+echo "" >> ../Makefile
+echo "run: App" >> ../Makefile
+echo -e "\t@echo \"==== Running App ====\"" >> ../Makefile
+echo -e "\t@./Binaries/linux-x86_64/\$(config)/App/App" >> ../Makefile
