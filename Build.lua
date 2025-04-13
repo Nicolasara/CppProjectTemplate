bdc85137ffbb2699ed54ff41e260fe590101bfe1
nicoaraujo <araujo.n@northeastern.edu>
2025-04-12 17:06:13 -0700

    Lowercase configuration names to in Build.lua

--- Build.lua
+++ Build.lua
@@ -1,7 +1,7 @@
 -- premake5.lua
 workspace "New Project"
    architecture "x64"
-   configurations { "Debug", "Release", "Dist" }
+   configurations { "debug", "release", "dist" }
    startproject "App"
 
    -- Workspace-wide build options for MSVC
